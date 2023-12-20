16005d14fa204fc1fa8096ce4c99325d41ea0f11
bwlwebdeveloper <153480714+bwlwebdeveloper@users.noreply.github.com>
2023-12-20 22:49:13 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,17 +1,17 @@
 <?php
-//get data from form  
+//get datafrom form  
 
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "bwlwebsitedeveloper@gmail.com";
-$subject = "Mail From website";
+$subject = "Mail From bwlwebsite";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: bwlwebsitedeveloper@gmail.com" . "\r\n" .
-"CC: abbupasha61@gmail.com";
+$headers = "From: noreply@bwlweb.com" . "\r\n" .
+"CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:index.html");
+header("Location:thankyou.html");
 ?>
