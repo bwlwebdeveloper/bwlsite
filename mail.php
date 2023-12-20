a2fc92e70627687cba7e1c3ef029166ed51dd3f3
bwlwebdeveloper <153480714+bwlwebdeveloper@users.noreply.github.com>
2023-12-20 22:39:05 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,8 +7,8 @@ $message= $_POST['message'];
 $to = "bwlwebsitedeveloper@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+$headers = "From: bwlwebsitedeveloper@gmail.com" . "\r\n" .
+"CC: abbupasha61@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
